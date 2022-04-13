eaecda64d9add99f107072b1997bbae0f11daf62
surajhimanshu <himanshu4suraj@gmail.com>
2022-04-14 04:42:34 +0530

    as

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const DB_URL = process.env.DB_URL || 80;
+const DB_URL = process.env.DB_URL || 1234;
 
 module.exports = () => {
   return mongoose.connect(DB_URL);
